221c153f1e04e0b807a0ded26d9d2f69ca2a3a6b
unknown <sthak8@DELTAHITI31110.sapient.com>
2015-09-04 11:27:11 +0530

    check class

--- src/com/sapient/client/cui/Check.java
+++ src/com/sapient/client/cui/Check.java
@@ -1,8 +1,18 @@
 package com.sapient.client.cui;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Check extends Payment implements Authorization {
 String name;
 int bankID;
+private final Map<String,Long> bankDetails = new HashMap<String,Long>();
+public final void validCardDetails(){
+	bankDetails.put("SBI",new Long(5196));
+	bankDetails.put("PNB",new Long(8523));
+	bankDetails.put("UNION BNAK", new Long(7897));  
+}
 public String getName() {
 	return name;
 }
@@ -15,10 +25,22 @@ public int getBankID() {
 public void setBankID(int bankID) {
 	this.bankID = bankID;
 }
-public boolean authorized()
+public String authorized()
 {
-	boolean authorize=true;
-	return authorize;
+	String authorize=" ";
+	Iterator mapIteration;
+	mapIteration = bankDetails.entrySet().iterator();
+	int bankID=this.getBankID();
+	String nameTest=this.getName();
+	while(mapIteration.hasNext())
+	{
+		Map.Entry mapEntry = (Map.Entry) mapIteration.next();
+		if(nameTest==(String)mapEntry.getValue()&& bankID==(Long)mapEntry.getKey());
+		{
+			return "authorised";
+		}
+	}
+	return "not authorized";
 }
 
 }
