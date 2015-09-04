7f704f2b8c02dbb8e00b5a9f22399801ba90187c
unknown <kmangl@DELTAHITI46582.sapient.com>
2015-09-04 11:12:07 +0530

    kkk

--- src/com/sapient/client/cui/OrderDetail.java
+++ src/com/sapient/client/cui/OrderDetail.java
@@ -1,11 +1,27 @@
 package com.sapient.client.cui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
 public class OrderDetail {
 
 int Quantity;
 boolean Taxstatus;
 Order order;
 Item Item;
+ArrayList<Integer> list=new ArrayList<Integer>();
+
+public void addItems()
+{
+	list.add(100);
+	list.add(200);
+	list.add(300);
+	list.add(400);
+	list.add(500);
+	
+}
 public int getQuantity() {
 	return Quantity;
 }
@@ -25,6 +41,7 @@ public void setOrder(Order order) {
 	this.order = order;
 }
 public Item getItem() {
+	
 	return Item;
 }
 public void setItem(Item item) {
@@ -33,9 +50,17 @@ public void setItem(Item item) {
 
 public void calcSubTotal()
 {
+	Iterator<Integer> listIterator=list.iterator();
+	while(listIterator.hasNext())
+	{
+		listIterator.next();
+		
+	}
+	
 }
 public void calcWeight()
 {
+	
 }
 
 }
